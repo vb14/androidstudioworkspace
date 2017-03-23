8433edd9cc9c2abe820a94eabbb0a5d831e190f4
Vishal Bhatnagar <vishal.bhatnagar.14@gmail.com>
2017-03-23 13:16:33 -0400

    Added GeoQuiz class

---
+++ GeoQuiz/app/src/main/java/com/vishal/bhatnagar/geoquiz/Question.java
@@ -0,0 +1,36 @@
+package com.vishal.bhatnagar.geoquiz;
+
+/**
+ * Created by vishalbhatnagar on 3/20/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public static void main (String[] args) {
+        System.out.println("HI!");
+    }
+}
